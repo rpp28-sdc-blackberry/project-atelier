26d322a576ab19fb03cddf840484ca2037738ecf
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-06-02 20:29:22 -0700

    installed react-testing-library, might need debugging

--- client/example.test.js
+++ client/example.test.js
@@ -1,3 +1,21 @@
+import React, {useState} from 'react';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+
+const CheckboxWithLabel = ({labelOn, labelOff}) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const onChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <label>
+      <input type="checkbox" checked={isChecked} onChange={onChange} />
+      {isChecked ? labelOn : labelOff}
+    </label>
+  );
+};
+
 var sum = (a, b) => {
   return a + b;
 };
@@ -14,4 +32,20 @@ describe('Google', () => {
   it('should be titled "Google"', async () => {
     await expect(page.title()).resolves.toMatch('Google');
   });
+});
+
+// Note: running cleanup afterEach is done automatically for you in @testing-library/react@9.0.0 or higher
+// unmount and cleanup DOM after the test is finished.
+// afterEach(cleanup);
+
+it('CheckboxWithLabel changes the text after click', () => {
+  const {queryByLabelText, getByLabelText} = render(
+    <CheckboxWithLabel labelOn="On" labelOff="Off" />,
+  );
+
+  expect(queryByLabelText(/off/i)).toBeTruthy();
+
+  fireEvent.click(getByLabelText(/off/i));
+
+  expect(queryByLabelText(/on/i)).toBeTruthy();
 });
