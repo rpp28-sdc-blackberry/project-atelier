294a849ad253450b5c14498d5e643c48886f9b0c
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-06-02 20:20:30 -0700

    installed puppeteer, might need to debug

--- client/example.test.js
+++ client/example.test.js
@@ -5,3 +5,13 @@ var sum = (a, b) => {
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+describe('Google', () => {
+  beforeAll(async () => {
+    await page.goto('https://google.com');
+  });
+
+  it('should be titled "Google"', async () => {
+    await expect(page.title()).resolves.toMatch('Google');
+  });
+});
