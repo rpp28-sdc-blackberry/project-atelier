d2d1e3def90adc37eb3a18cde0b0daafb1c67b03
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-06-06 16:30:56 -0700

    create ReviewsList component

--- client/ratings_reviews/RatingsReviews.jsx
+++ client/ratings_reviews/RatingsReviews.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ReviewsList from './components/ReviewsList.jsx';
 
 const RatingsReviews = (props) => (
   <div>
     ratingsreviews
+    <ReviewsList />
   </div>
 );
 
---
+++ client/ratings_reviews/components/ReviewsList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ReviewsList = (props) => (
+  <div>
+    reviewslist
+  </div>
+);
+
+export default ReviewsList;
