3bd92cb1c5b64dc3bc9d7a774fa9d282f00b1b75
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-06-06 15:26:25 -0700

    implemented wildcard route

--- server/index.js
+++ server/index.js
@@ -4,10 +4,18 @@ const app = express();
 const port = 8080;
 
 app.use(express.static('public'));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendStatus(200);
+app.all('*', (req, res) => {
+  console.log('req.method:', req.method);
+  console.log('req.url', req.url);
+  console.log('req.body', req.body);
+  res.status(200).send('got your request');
 });
+// // this get route is only used for an example test...
+// app.get('/', (req, res) => {
+//   res.sendStatus(200);
+// });
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
