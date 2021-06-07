ece59bf4febd3b3657db9a6197bca616537b5bda
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-06-07 11:36:44 -0700

    implemented development server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
-
 const app = express();
 const port = 8080;
-
+const queryAPI = require('./atelier.js');
 app.use(express.static('public'));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendStatus(200);
+app.all('*', (req, res) => {
+
+  queryAPI(req.method, req.url, req.body)
+    .then((response) => {
+      let { statusCode } = response.request.res;
+      res.status(statusCode).send(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send(err);
+    });
+
 });
 
 app.listen(port, () => {
