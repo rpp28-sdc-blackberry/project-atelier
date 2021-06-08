5442c83de287b35d6367ee03b7ca305e77f44205
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-06-08 13:30:06 -0700

    set showRecommend and showResponse in ReviewTile's state

--- client/ratings_reviews/components/ReviewTile.jsx
+++ client/ratings_reviews/components/ReviewTile.jsx
@@ -6,23 +6,21 @@ class ReviewTile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      rating: this.props.review.rating
+      rating: this.props.review.rating,
+      showRecommend: this.props.review.recommend,
+      showResponse: !(this.props.review.response === null || this.props.review.response.length === 0)
     };
   }
 
   render() {
-    var responseVisibility = true;
-    if (this.props.review.response === null || this.props.review.response.length === 0) {
-      responseVisibility = false;
-    }
     return (
       <div class='review-tile'>
         <div class="stars" style={{'--rating': this.state.rating}}></div>
         <div>{this.props.review.reviewer_name}, {this.props.review.date.slice(0, 10)}</div>
         <div>{this.props.review.summary}</div>
         <div>{this.props.review.body}</div>
-        <div hidden={!this.props.review.recommend}>I recommend this product!</div>
-        <div hidden={!responseVisibility}>Response: {this.props.review.response}</div>
+        <div hidden={!this.state.showRecommend}>I recommend this product!</div>
+        <div hidden={!this.state.showResponse}>Response: {this.props.review.response}</div>
         <div><span>Helpful? Yes {this.props.review.helpfulness} | Report</span></div>
       </div>
     );
