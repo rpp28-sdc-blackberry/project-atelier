2372d3307711a77280caae5e478e9d0d5a27fb96
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-06-08 15:31:11 -0700

    add formatDate helper for RatingsReviews

--- client/ratings_reviews/components/ReviewTile.jsx
+++ client/ratings_reviews/components/ReviewTile.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import helpers from '../helpers.js';
 
 class ReviewTile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       rating: this.props.review.rating,
+      name: this.props.review.reviewer_name,
+      date: helpers.formatDate(this.props.review.date.slice(0, 10)),
+      summary: this.props.review.summary,
+      body: this.props.review.body,
+      response: this.props.review.response,
+      helpfulness: this.props.review.helpfulness,
       showRecommend: this.props.review.recommend,
       showResponse: !(this.props.review.response === null || this.props.review.response.length === 0)
     };
@@ -16,12 +23,12 @@ class ReviewTile extends React.Component {
     return (
       <div class='review-tile'>
         <div class="stars" style={{'--rating': this.state.rating}}></div>
-        <div>{this.props.review.reviewer_name}, {this.props.review.date.slice(0, 10)}</div>
-        <div class='review-summary'>{this.props.review.summary}</div>
-        <div class='review-body'>{this.props.review.body}</div>
+        <div>{this.state.name}, {this.state.date}</div>
+        <div class='review-summary'>{this.state.summary}</div>
+        <div class='review-body'>{this.state.body}</div>
         <div class='user-recommend' hidden={!this.state.showRecommend}>I recommend this product!</div>
-        <div class='seller-response' hidden={!this.state.showResponse}>Response: {this.props.review.response}</div>
-        <div><span>Helpful? Yes ({this.props.review.helpfulness}) | Report</span></div>
+        <div class='seller-response' hidden={!this.state.showResponse}>Response: {this.state.response}</div>
+        <div><span>Helpful? Yes ({this.state.helpfulness}) | Report</span></div>
       </div>
     );
   }
