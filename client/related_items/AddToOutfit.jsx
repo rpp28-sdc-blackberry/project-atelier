ab5a2b03dbb1a10455c9adad8ac125f9b70ab190
jabrake <jorge.brake@gmail.com>
2021-06-07 17:04:58 -0700

    Created components for Related Items widget

---
+++ client/related_items/AddToOutfit.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddToOutfit = (props) => (
+  <div>
+    add to outfit
+  </div>
+);
+
+export default AddToOutfit;
---
+++ client/related_items/ItemCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ItemCard = (props) => (
+  <div>
+    item card
+  </div>
+);
+
+export default ItemCard;
