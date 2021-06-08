7344a984801d6e01272814b3c51947ce8695718b
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-06-07 20:44:53 -0700

    basic setup for review tile component and star rating feature (WIP), routing removed

--- client/ratings_reviews/components/ReviewTile.jsx
+++ client/ratings_reviews/components/ReviewTile.jsx
@@ -1,59 +1,42 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
-// class ReviewTile extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       recommend: this.props.review.recommend,
-//       response: tris.props.review
-//     };
-//   }
-
-//   formatDate() {
-//     // format the review date
-//   }
-
-//   toggleRecommend() {
-//     // if recommend is false, then toggle off the visiblity
-//   }
-
-//   toggleResponse() {
-//     // if response is empty or null, then toggle off the visibility
-//   }
-
-//   render() {
-//     return (
-//       <div class='review-tile'>
-//         <div>Rating: {props.review.rating}</div>
-//         <div>{props.review.reviewer_name}, {props.review.date.slice(0, 10)}</div>
-//         <div>{props.review.summary}</div>
-//         <div>{props.review.body}</div>
-//         <div hidden={props.review.recommend}>Recommend: {props.review.recommend}</div>
-//         <div hideen={props.review.response}>Response: {props.review.response}</div>
-//         <div><span>Helpful? Yes {props.review.helpfulness} | Report</span></div>
-//       </div>
-
-//     );
-//   }
-// }
-
-const ReviewTile = (props) => {
-  var responseVisibility = true;
-  if (props.review.response === null || props.review.response.length === 0) {
-    responseVisibility = false;
+class ReviewTile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rating: 0
+    };
+    this.updateStars = this.updateStars.bind(this);
   }
-  return (
-    <div class='review-tile'>
-      <div>Rating: {props.review.rating}</div>
-      <div>{props.review.reviewer_name}, {props.review.date.slice(0, 10)}</div>
-      <div>{props.review.summary}</div>
-      <div>{props.review.body}</div>
-      <div hidden={!props.review.recommend}>I recommend this product!</div>
-      <div hidden={!responseVisibility}>Response: {props.review.response}</div>
-      <div><span>Helpful? Yes {props.review.helpfulness} | Report</span></div>
-    </div>
-  );
-};
 
+  updateStars() {
+    var percentage = (this.props.review.rating / 5) * 100;
+    var percentageRounded = (Math.round(percentage * 4) / 4).toFixed(2);
+  }
+
+  render() {
+    var responseVisibility = true;
+    if (this.props.review.response === null || this.props.review.response.length === 0) {
+      responseVisibility = false;
+    }
+    return (
+      <div class='review-tile'>
+        <div id={this.props.review.review_id}>Rating: {this.props.review.rating}
+          <div class='stars-outer'>
+            <div class='stars-inner'></div>
+          </div>
+        </div>
+        <div>{this.props.review.reviewer_name}, {this.props.review.date.slice(0, 10)}</div>
+        <div>{this.props.review.summary}</div>
+        <div>{this.props.review.body}</div>
+        <div hidden={!this.props.review.recommend}>I recommend this product!</div>
+        <div hidden={!responseVisibility}>Response: {this.props.review.response}</div>
+        <div><span>Helpful? Yes {this.props.review.helpfulness} | Report</span></div>
+      </div>
+    );
+  }
+}
 
 export default ReviewTile;
---
+++ client/ratings_reviews/components/StarRating.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StarRating = () => {
+  return (
+    <div>
+      {[...Array(5)].map(star => {
+        // this method requires react-icons
+      })}
+    </div>
+  );
+};
+
+export default StarRating;
