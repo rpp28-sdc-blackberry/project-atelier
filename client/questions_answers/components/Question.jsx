63e66a82898dd63a9eb7529bc969976e0eefa4e5
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-06-10 10:41:44 -0700

    implementing answer form

--- client/questions_answers/components/Question.jsx
+++ client/questions_answers/components/Question.jsx
@@ -5,6 +5,7 @@ const Question = (props) => (
 
   <div className="qa-item">
     <b> {`Q: ${props.question}`} </b>
+    <button onClick={() => console.log(props.question_id)}> Add Answer </button>
     <AnswersList answers={props.answers}/>
   </div>
 
