84032bcd28c59a3de7cfd08e3c32659ee48a33a2
jabrake <jorge.brake@gmail.com>
2021-06-12 12:05:33 -0700

    Added basic tests for all components

--- client/related_items/tests/unitTests.test.js
+++ client/related_items/tests/unitTests.test.js
@@ -5,9 +5,37 @@
 import React from 'react';
 import {cleanup, fireEvent, render} from '@testing-library/react';
 import RelatedItems from '../RelatedItems.jsx';
+import ItemsList from '../ItemsList.jsx';
+import ItemCard from '../ItemCard.jsx';
+import ComparisonModal from '../ComparisonModal.jsx';
+import AddToOutfit from '../AddToOutfit.jsx';
 
 describe('RelatedItems component should exist', () => {
   it('should exist', () => {
     expect(<RelatedItems />).toBeTruthy;
+  });
+});
+
+describe('ItemsList component should exist', () => {
+  it('should exist', () => {
+    expect(<ItemsList />).toBeTruthy;
+  });
+});
+
+describe('ItemCard component should exist', () => {
+  it('should exist', () => {
+    expect(<ItemCard />).toBeTruthy;
+  });
+});
+
+describe('ComparisonModal component should exist', () => {
+  it('should exist', () => {
+    expect(<ComparisonModal />).toBeTruthy;
+  });
+});
+
+describe('AddToOutfit component should exist', () => {
+  it('should exist', () => {
+    expect(<AddToOutfit />).toBeTruthy;
   });
 });
