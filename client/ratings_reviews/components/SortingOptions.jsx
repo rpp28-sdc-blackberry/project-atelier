5404d16bc095f80d6a7869bb424c59091715ed8d
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-06-11 18:20:08 -0700

    change SortingOptions to stateful component

--- client/ratings_reviews/components/SortingOptions.jsx
+++ client/ratings_reviews/components/SortingOptions.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 
-const SortingOptions = (props) => (
-  <div>
-    <label for="options">[Number] reviews, sorted by </label>
-    <select name="options" id="sorting-options">
-      <option selected='selected' value="relevance">Relevant</option>
-      <option value="helpfulness">Helpful</option>
-      <option value="date">Newnest</option>
-    </select>
-  </div>
-);
+class SortingOptions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <label for="options">[Number] reviews, sorted by </label>
+        <select name="options" id="sorting-options">
+          <option selected='selected' value="relevance">Relevant</option>
+          <option value="helpfulness">Helpful</option>
+          <option value="date">Newnest</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+// const SortingOptions = (props) => (
+//   <div>
+//     <label for="options">[Number] reviews, sorted by </label>
+//     <select name="options" id="sorting-options">
+//       <option selected='selected' value="relevance">Relevant</option>
+//       <option value="helpfulness">Helpful</option>
+//       <option value="date">Newnest</option>
+//     </select>
+//   </div>
+// );
 
 export default SortingOptions;
