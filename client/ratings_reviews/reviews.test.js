82663bcf6f6a2a8cebd25cb2f9b8c6851733f153
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-06-14 12:26:05 -0700

    add some testing structure

--- client/ratings_reviews/reviews.test.js
+++ client/ratings_reviews/reviews.test.js
@@ -1,8 +1,12 @@
 import React, {useState} from 'react';
-import TestRenderer from 'react-testing-renderer';
 import {cleanup, fireEvent, render, screen} from '@testing-library/react';
 
 import RatingsReviews from './RatingsReviews.jsx';
+import ReviewsList from './components/ReviewsList.jsx';
+import ReviewTile from './components/ReviewTile.jsx';
+import RatingBreakdown from './components/RatingBreakdown.jsx';
+import ProductBreakdown from './components/ProductBreakdown.jsx';
+import SortingOptions from './components/SortingOptions.jsx';
 
 describe('RatingsReviews component', () => {
 
@@ -10,10 +14,44 @@ describe('RatingsReviews component', () => {
     expect(<RatingsReviews />).toBeTruthy;
   });
 
-  it('should...', () => {
-    const testRenderer = TestRenderer.create(<RatingsReviews />);
-    const testInstance = testRenderer.root;
+});
 
+describe('ReviewsList component', () => {
+
+  it('should exist', () => {
+    expect(<RatingsReviews />).toBeTruthy;
+  });
+
+});
+
+describe('ReviewTile component', () => {
+
+  it('should exist', () => {
+    expect(<ReviewTile />).toBeTruthy;
+  });
+
+});
+
+describe('RatingBreakdown component', () => {
+
+  it('should exist', () => {
+    expect(<RatingBreakdown />).toBeTruthy;
+  });
+
+});
+
+describe('ProductBreakdown component', () => {
+
+  it('should exist', () => {
+    expect(<ProductBreakdown />).toBeTruthy;
+  });
+
+});
+
+describe('SortingOptions component', () => {
+
+  it('should exist', () => {
+    expect(<SortingOptions />).toBeTruthy;
   });
 
 });
