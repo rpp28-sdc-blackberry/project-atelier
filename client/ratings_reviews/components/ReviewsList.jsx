1175b2153cce959717021a94fbde49bd694b1144
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-06-14 17:06:01 -0700

    add scrolling feature for reviews list

--- client/ratings_reviews/components/ReviewsList.jsx
+++ client/ratings_reviews/components/ReviewsList.jsx
@@ -83,7 +83,7 @@ class ReviewsList extends React.Component {
     if (this.state.allReviews.length !== 0) {
       return (
         <div key={this.props.reviews[0].review_id} class='reviews-list'>
-          <div>{this.state.currentReviews.map(review => <ReviewTile review={review}/>)}</div>
+          <div class='reviews-list-tiles'>{this.state.currentReviews.map(review => <ReviewTile review={review}/>)}</div>
           <button class='review-show-button' onClick={this.showMoreReviews} hidden={!this.state.showMoreReviewsButton}>More Reviews</button>
           <button class='review-show-button' onClick={this.showLessReviews} hidden={!this.state.showLessReviewsButton}>Less Reviews</button>
         </div>
