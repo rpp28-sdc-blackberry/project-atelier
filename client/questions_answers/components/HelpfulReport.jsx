52935375487c49e28147ff0e8ae87d3ebba6d852
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-06-15 10:26:39 -0700

    refactored helpful-report to component

---
+++ client/questions_answers/components/HelpfulReport.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const HelpfulReport = (props) => {
+
+  let renderReportedLink, renderHelpfulLink;
+
+  if (props.reported) {
+    renderReportedLink = (<span> Reported </span>);
+  } else {
+    renderReportedLink = (<button onClick={props.handleReportButtonClick}> Report </button>);
+  }
+
+  let helpfulQuestions = JSON.parse(localStorage.getItem('helpfulQuestions'));
+
+  if (helpfulQuestions.includes(props.question_id)) {
+    renderHelpfulLink = (<span> {`Helpful? Yes ${props.helpfulness}`}</span>);
+  } else {
+    renderHelpfulLink = (<span>
+      <span> Helpful? </span>
+      <button onClick={props.handleHelpfulButtonClick}> {`Yes ${props.helpfulness}`} </button>
+    </span>);
+  }
+
+  return (
+    <span>
+      {renderHelpfulLink}
+      {renderReportedLink}
+    </span>
+  );
+
+};
+
+
+export default HelpfulReport;
