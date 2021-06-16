9f70ee8c2fc0d6ccf4dcc84ceb8556ed1ca2c377
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-06-15 19:34:58 -0700

    refactored HelpfulReport to be shorter

--- client/questions_answers/components/HelpfulReport.jsx
+++ client/questions_answers/components/HelpfulReport.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const HelpfulReport = (props) => {
 
   let renderReportedLink, renderHelpfulLink;
+  let helpfulIds = JSON.parse(localStorage.getItem(props.content_type === 'question' ? 'helpfulQuestions' : 'helpfulAnswers'));
 
   if (props.reported) {
     renderReportedLink = (<span> Reported </span>);
@@ -10,31 +11,13 @@ const HelpfulReport = (props) => {
     renderReportedLink = (<button onClick={props.handleReportButtonClick}> Report </button>);
   }
 
-  if (props.content_type === 'question') {
-    let helpfulQuestions = JSON.parse(localStorage.getItem('helpfulQuestions'));
-
-    if (helpfulQuestions.includes(props.content_id)) {
-      renderHelpfulLink = (<span> {`Helpful? Yes ${props.helpfulness}`}</span>);
-    } else {
-      renderHelpfulLink = (<span>
-        <span> Helpful? </span>
-        <button onClick={props.handleHelpfulButtonClick}> {`Yes ${props.helpfulness}`} </button>
-      </span>);
-    }
-  }
-
-  if (props.content_type === 'answer') {
-    let helpfulAnswers = JSON.parse(localStorage.getItem('helpfulAnswers'));
-
-    if (helpfulAnswers.includes(props.content_id)) {
-      renderHelpfulLink = (<span> {`Helpful? Yes ${props.helpfulness}`}</span>);
-    } else {
-      renderHelpfulLink = (<span>
-        <span> Helpful? </span>
-        <button onClick={props.handleHelpfulButtonClick}> {`Yes ${props.helpfulness}`} </button>
-      </span>);
-    }
-
+  if (helpfulIds.includes(props.content_id)) {
+    renderHelpfulLink = (<span> {`Helpful? Yes ${props.helpfulness}`}</span>);
+  } else {
+    renderHelpfulLink = (<span>
+      <span> Helpful? </span>
+      <button onClick={props.handleHelpfulButtonClick}> {`Yes ${props.helpfulness}`} </button>
+    </span>);
   }
 
   return (
@@ -45,6 +28,5 @@ const HelpfulReport = (props) => {
   );
 
 };
-
 
 export default HelpfulReport;
