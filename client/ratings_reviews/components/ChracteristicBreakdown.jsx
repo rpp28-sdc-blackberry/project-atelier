aa8fbf2a419d26563d311a9347d54daaa7876aec
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-06-18 20:40:24 -0700

    change the slider bar and styling for product breakdown

--- client/ratings_reviews/components/ChracteristicBreakdown.jsx
+++ client/ratings_reviews/components/ChracteristicBreakdown.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import helpers from '../helpers.js';
 
 const CharacteristicBreakdown = (props) => (
   <div>
     <label>{props.characteristic.name}</label><br></br>
-    <meter min='0' max='5' low='1' high='4' optimum='2.5' value={props.characteristic.value}></meter>
+    <div class='review-product-breakdown-container'>
+      <div>
+        <input class='review-product-breakdown-bar' type='range' min='1' max='5' value={props.characteristic.value}></input>
+      </div>
+      <div class='review-product-breakdown-description-container'>
+        {[1, 3, 5].map(key =>
+          <span class='review-product-breakdown-description'>{helpers.productCharacteristics[props.characteristic.name][key]}</span>
+        )}
+      </div>
+    </div>
   </div>
 );
 
