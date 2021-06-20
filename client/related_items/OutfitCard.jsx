88274b5c2cf09c33457b88ec9a4feccb9eaf44fd
jabrake <jorge.brake@gmail.com>
2021-06-19 17:30:54 -0700

    Refactored OutfitCard to be a functional stateless component

--- client/related_items/OutfitCard.jsx
+++ client/related_items/OutfitCard.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 
-class OutfitCard extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.removeFromOutfit = this.props.removeFromOutfit.bind(this);
-  }
-
-  render() {
-    return (
-      <div className='relatedItemCard' id={this.props.productInfo.id}>
-        <div id="action" onClick={this.props.removeFromOutfit}>Remove</div>
-        <img id="thumbnail" src={this.props.productInfo.thumbnailUrl}></img>
-        <p id="category">{this.props.productInfo.category}</p>
-        <p id="name">{this.props.productInfo.name}</p>
-        <p id="price">{this.props.productInfo.price}</p>
-        <p id="rating">{this.props.productInfo.rating}</p>
-      </div>
-    );
-  }
-}
+const OutfitCard = (props) => (
+  <div className='relatedItemCard' id={props.productInfo.id}>
+    <div id="action" onClick={(e) => props.removeFromOutfit(e)}>Remove</div>
+    <img id="thumbnail" src={props.productInfo.thumbnailUrl}></img>
+    <p id="category">{props.productInfo.category}</p>
+    <p id="name">{props.productInfo.name}</p>
+    <p id="price">{props.productInfo.price}</p>
+    <p id="rating">{props.productInfo.rating}</p>
+  </div>
+);
 
 export default OutfitCard;
