940eb0c97101f7d2679063ce8b4d0e40ff724548
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-06-21 10:01:09 -0700

    fixed bug in making answers helpful

--- client/questions_answers/components/AnswersList.jsx
+++ client/questions_answers/components/AnswersList.jsx
@@ -52,9 +52,7 @@ class AnswersList extends React.Component {
   }
 
   incrementHelpfulAnswer(answerId) {
-    // if showCollapseAnswersList button is true
     if (this.state.showCollapseAnswersListButton) {
-      // find the answer in this.state.answers and increment its helpfulness
       this.state.answers.forEach((answer) => {
         if (answer.id === answerId) {
           answer.helpfulness++;
@@ -70,7 +68,12 @@ class AnswersList extends React.Component {
       this.state.showAnswersList &&
       <div className="qa-answers-list-container">
         <b>A:</b>
-        {this.state.answers.map((answer) => <Answer key={answer.id} answer={answer} helpfulness={answer.helpfulness} answer_id={answer.id} incrementHelpfulAnswer={this.incrementHelpfulAnswer}/>)}
+        {this.state.answers.map((answer) => <Answer
+          key={answer.id} answer={answer}
+          helpfulness={answer.helpfulness}
+          answer_id={answer.id}
+          incrementHelpfulAnswer={this.incrementHelpfulAnswer}
+        />)}
         {this.state.showLoadMoreAnswersButton && <button onClick={this.loadRemainingAnswers}> See more answers </button>}
         {this.state.showCollapseAnswersListButton && <button onClick={this.collapseAnswersList}> Collapse answers </button>}
       </div>
