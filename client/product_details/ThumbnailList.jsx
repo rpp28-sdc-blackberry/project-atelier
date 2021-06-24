da9ae92229f53dbd5fc602c7a2c60ed626072d99
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-06-23 18:39:29 -0700

    added two vars to fix bug in ThumbnailList

--- client/product_details/ThumbnailList.jsx
+++ client/product_details/ThumbnailList.jsx
@@ -28,34 +28,34 @@ class ThumbnailList extends React.Component {
       firstPhotoIndex: newIndex
     });
   }
-  
+
   render() {
     if (this.props.selectedStyle) {
       var photos = this.props.selectedStyle.photos.slice(this.state.firstPhotoIndex, this.state.firstPhotoIndex + 7);
       if (photos.length < 7) {
         var remainder = this.props.selectedStyle.photos.slice(0, (7 - photos.length));
         photos = photos.concat(remainder);
-      } 
+      }
 
       return (
         <div id="thumbnailList" className={this.props.view}>
-          {this.props.selectedStyle.photos.length > 7 ? 
+          {this.props.selectedStyle.photos.length > 7 ?
             <a onClick={this.handleUpScroll} id="upScroll-container">
               <i id="upScroll" class="fa fa-angle-up"></i>
-            </a> : null}    
+            </a> : null}
           <br></br>
           {photos.map((photo, index) => {
             var initIndex = index;
             if ((initIndex + this.state.firstPhotoIndex) > this.props.selectedStyle.photos.length) {
-              finalIndex = initIndex + this.state.firstPhotoIndex - this.props.selectedStyle.photos.length - 2;
+              var finalIndex = initIndex + this.state.firstPhotoIndex - this.props.selectedStyle.photos.length - 2;
             } else {
-              finalIndex = initIndex + this.state.firstPhotoIndex;
+              var finalIndex = initIndex + this.state.firstPhotoIndex;
             }
             return (
-              <Thumbnail 
-                name={this.props.selectedStyle.name} 
-                photo={photo} 
-                index={finalIndex} 
+              <Thumbnail
+                name={this.props.selectedStyle.name}
+                photo={photo}
+                index={finalIndex}
                 indexSelected={this.props.currPhotoIndex}
                 handlePhotoSelection={this.props.handlePhotoSelection}
                 view={this.props.view}/>
@@ -67,10 +67,10 @@ class ThumbnailList extends React.Component {
             </a> : null}
         </div>
       );
-    } 
+    }
     return null;
   }
-  
+
 }
 
 export default ThumbnailList;
