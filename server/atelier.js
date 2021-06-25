23911eb4692e262d2e6558c10732a75cadf8299c
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-06-25 01:36:17 -0700

    add .env to gitignore

--- server/atelier.js
+++ server/atelier.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
-const TOKEN = require('../config.js');
+require('dotenv').config();
 
 const queryAPI = (method, url, body) => {
 
   return axios({
     headers: {
-      Authorization: TOKEN
+      Authorization: process.env.TOKEN
     },
     method: method,
     url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp${url}`,
