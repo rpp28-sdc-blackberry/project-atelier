5693cef39c10a44528260b22cfb1922e0c6386a3
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-06-26 15:48:45 -0700

    reversed view of questions list on load

--- client/questions_answers/components/QuestionsList.jsx
+++ client/questions_answers/components/QuestionsList.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import Question from './Question.jsx';
 
 const QuestionsList = (props) => (
-  <div className="qa-questions-list-container">
-    {props.questions.map((question) => <Question
-      key={question.question_id}
-      question={question.question_body}
-      helpfulness={question.question_helpfulness}
-      answers={question.answers}
-      question_id={question.question_id}
-      name={props.name}
-      handleAddAnswerClick={props.handleAddAnswerClick}
-    />)}
+  <div className="qa-questions-list-container-wrapper">
+    <div className="qa-questions-list-container">
+      {props.questions.map((question) => <Question
+        key={question.question_id}
+        question={question.question_body}
+        helpfulness={question.question_helpfulness}
+        answers={question.answers}
+        question_id={question.question_id}
+        name={props.name}
+        handleAddAnswerClick={props.handleAddAnswerClick}
+      />)}
+    </div>
   </div>
 
 );
