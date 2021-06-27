1d14786fb5423c4b14ffae287039e9aac7c8c756
jabrake <jorge.brake@gmail.com>
2021-06-27 10:11:50 -0700

    Changed id to class for action buttons, added new ids

--- client/related_items/OutfitCard.jsx
+++ client/related_items/OutfitCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const OutfitCard = (props) => (
   <div className='rp-card' id={props.productInfo.id} onClick={() => props.handleRelatedItemClick(props.productInfo.id)}>
     <div id='rp-action-container'>
-      <div id='rp-card-action' onClick={(e) => props.removeFromOutfit(e)}>Remove</div>
+      <div className='rp-card-action' id='rp-card-remove' onClick={(e) => props.removeFromOutfit(e)}>Remove</div>
     </div>
     <div id='rp-thumbnail-container'>
       <img id='rp-thumbnail-image' src={props.productInfo.thumbnailUrl}></img>
