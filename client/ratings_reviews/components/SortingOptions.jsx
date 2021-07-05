91bf621cb0d2a8841927d3149bae2a4778f3f349
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-07-05 02:02:57 -0700

    remove handleChange

--- client/ratings_reviews/components/SortingOptions.jsx
+++ client/ratings_reviews/components/SortingOptions.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const SortingOptions = (props) => {
-  const handleChange = (e) => {
-    props.handleOptionChanges(e.target.value);
-  };
   return (
     <div class='review-sorting-options'>
       <label for="options">{props.reviews.length} reviews, sorted by </label>
-      <select value={props.sortingOption} onChange={handleChange} name='options' id='sorting-options' class='review-clickable'>
+      <select
+        value={props.sortingOption}
+        onChange={(e) => props.handleOptionChanges(e.target.value)}
+        name='options'
+        id='sorting-options'
+        class='review-clickable'>
         <option selected='selected' value="relevance">Relevant</option>
         <option value="helpfulness">Helpful</option>
         <option value="date">Newest</option>
